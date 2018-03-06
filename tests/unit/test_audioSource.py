f2b33d48b6ba210b2811b422fc5cdc8a81819b07
Valter Akira Miasato Fº <valterf@cpqd.com.br>
2018-03-06 16:25:27 -0300

    Adiciona teste que checa equivalência entre audio sources de arquivo e buffer

---
+++ tests/unit/test_audioSource.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+#
+#  Copyright 2017 CPqD. All rights reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+"""
+@author: Akira Miasato
+
+Tests with audio sources
+"""
+
+from cpqdasr.speech_recognizer import SpeechRecognizer, LanguageModelList
+from cpqdasr.audio_source import FileAudioSource, BufferAudioSource
+from .config import url, credentials, slm, phone_wav
+from .config import log_level, log_path
+
+import soundfile as sf
+
+
+asr_kwargs = {'credentials': credentials,
+              'logLevel': log_level,
+              'logStream': open(log_path, 'w')}
+
+
+# =============================================================================
+# Test cases
+# =============================================================================
+def testEquivalenceFileBuffer():
+
+    # File
+    asr = SpeechRecognizer(url, **asr_kwargs)
+    asr.recognize(FileAudioSource(phone_wav),
+                  LanguageModelList(slm))
+    result_file = asr.waitRecognitionResult()[0].alternatives[0]['text']
+    asr.close()
+
+    # Buffer
+    asr = SpeechRecognizer(url, **asr_kwargs)
+    source = BufferAudioSource()
+    asr.recognize(source,
+                  LanguageModelList(slm))
+    sig, rate = sf.read(phone_wav)
+    source.write((sig * 2**15).astype('int16').tobytes())
+    source.finish()
+    result_buffer = asr.waitRecognitionResult()[0].alternatives[0]['text']
+    asr.close()
+
+    assert result_file == result_buffer
